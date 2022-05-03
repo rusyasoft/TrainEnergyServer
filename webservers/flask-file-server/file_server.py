fcfd13f59b4f7f0277276bce159356d041a7d1c3
Porcupiney Hairs <porucpiney.hairs@protonmail.com>
2022-05-03 18:37:46 +0530

    # Absolute Path Traversal due to incorrect use of `send_file` call
    A path traversal attack (also known as directory traversal) aims to access files and directories that are stored outside the web root folder. By manipulating variables that reference files with “dot-dot-slash (../)” sequences and its variations or by using absolute file paths, it may be possible to access arbitrary files and directories stored on file system including application source code or configuration and critical system files. This attack is also known as “dot-dot-slash”, “directory traversal”, “directory climbing” and “backtracking”.
    
    ## Common Weakness Enumeration category
    CWE - 36
    
    ## Root Cause Analysis
    
    The `os.path.join` call is unsafe for use with untrusted input. When the `os.path.join` call encounters an absolute path, it ignores all the parameters it has encountered till that point and starts working with the new absolute path.  Please see the example below.
    ```
    >>> import os.path
    >>> static = "path/to/mySafeStaticDir"
    >>> malicious = "/../../../../../etc/passwd"
    >>> os.path.join(t,malicious)
    '/../../../../../etc/passwd'
    ```
    Since the "malicious" parameter represents an absolute path, the result of `os.path.join` ignores the static directory completely. Hence, untrusted input is passed via the `os.path.join` call to `flask.send_file` can lead to path traversal attacks.
    
    In this case, the problems occurs due to the following code :
    https://github.com/rusyasoft/TrainEnergyServer/blob/180fb127b791300f886e8ac95c84c67a9b869ecd/webservers/flask-file-server/file_server.py#L133
    
    Here, the `p` parameter is attacker controlled. This parameter passes through the unsafe `os.path.join` call making the effective directory and filename passed to the `send_file` call attacker controlled. This leads to a path traversal attack.
    
    ## Proof of Concept
    
    The bug can be verified using a proof of concept similar to the one shown below.
    
    ```
    curl --path-as-is 'http://<domain>/download//../../../../etc/passwd"'
    ```
    ## Remediation
    
    This can be fixed by preventing flow of untrusted data to the vulnerable `send_file` function. In case the application logic necessiates this behaviour, one can either use the `werkzeug.utils.safe_join` to join untrusted paths or replace `flask.send_file` calls with `flask.send_from_directory` calls.
    
    ## Common Vulnerability Scoring System Vector
    
    The attack can be carried over the network. A complex non-standard configuration or a specialized condition is not required for the attack to be successfully conducted. There is no user interaction required for successful execution. The attack can affect components outside the scope of the target module. The attack can be used to gain access to confidential files like passwords, login credentials and other secrets. It cannot be directly used to affect a change on a system resource. Hence has limited to no impact on integrity. Using this attack vector a attacker may make multiple requests for accessing huge files such as a database. This can lead to a partial system denial service. However, the impact on availability is quite low in this case. Taking this account an appropriate CVSS v3.1 vector would be
    
    (AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:N/A:L)[https://nvd.nist.gov/vuln-metrics/cvss/v3-calculator?vector=AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:N/A:L&version=3.1]
    
    This gives it a base score of 9.3/10 and a severity rating of critical.
    
    ## References
    * [OWASP Path Traversal](https://owasp.org/www-community/attacks/Path_Traversal)
    * github/securitylab#669
    
    ### This bug was found using *[CodeQL by Github](https://codeql.github.com/)*

--- webservers/flask-file-server/file_server.py
+++ webservers/flask-file-server/file_server.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, request, session, render_template, send_file, Response
 from flask.views import MethodView
-from werkzeug import secure_filename
+from werkzeug import secure_filename, safe_join
 from datetime import datetime
 import humanize
 import os
@@ -101,7 +101,7 @@ class PathView(MethodView):
     def get(self, p=''):
         hide_dotfile = request.args.get('hide-dotfile', request.cookies.get('hide-dotfile', 'no'))
 
-        path = os.path.join(root, p)
+        path = safe_join(root, p)
         if os.path.isdir(path):
             contents = []
             total = {'size': 0, 'dir': 0, 'file': 0}
